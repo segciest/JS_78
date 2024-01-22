f0d411c9bc5cc815f39f86a833ea5963d3a15c03
segciest <minhduy1705@gmail.com>
2024-01-22 22:48:03 +0700

    2nd push

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ function nhapMang(arrItem) {
     arr.push(arrItem);
     console.log(arr);
 }
+document.getElementById('btn_nhapMang').onclick = function(){
+    var arrItem = document.getElementById('txt_nhapMang').value*1;
+    nhapMang(arrItem);
+    document.getElementById('kqMang').innerHTML = `Mảng hiện tại: ${arr}`;
+};
+
+
+
 // Tổng số dương
 function tongSoDuong(arr){
     var sum =0;
@@ -14,6 +22,12 @@ function tongSoDuong(arr){
     }
     return sum;
 }
+document.getElementById('btn_tongDuong').onclick = function (){
+    document.getElementById('kq').innerHTML = `Tổng số dương trong mảng: ${tongSoDuong(arr)}`
+}
+
+
+
 // Đếm số dương
 function demSoDuong(arr){
     var count =0;
@@ -24,7 +38,31 @@ function demSoDuong(arr){
     }
     return count;
 }
+document.getElementById('btn_demSoDuong').onclick = function (){
+    document.getElementById('kq').innerHTML = `Có ${demSoDuong(arr)} số dương trong mảng`
+}
+
+
+
+// Sắp xếp mảng (tăng dần)
+function sortTangDan (arr){
+    arr.sort(function(a,b){
+        return a-b;
+    })
+    return arr;
+}
+// Tănh dần
+document.getElementById('btn_tangDan').onclick = function(){
+    document.getElementById('kq').innerHTML = sortTangDan(arr);
+}
 // Tìm số nhỏ nhất (dùng function sortTangDan)
+document.getElementById('btn_soNhoNhat').onclick = function (){
+    document.getElementById('kq').innerHTML = sortTangDan(arr)[0];
+}
+
+
+
+// Tìm số dương nhỏ nhất trong mảng
 function soDuongnhoNhat (arr){
     for (var i=0;i<arr.length;i++){
         if (arr[i]>0){
@@ -32,6 +70,12 @@ function soDuongnhoNhat (arr){
         }
     }
 }
+document.getElementById('btn_soDuongNhoNhat').onclick = function (){
+    document.getElementById('kq').innerHTML = `Số dương nhỏ nhất trong mảng là:${soDuongnhoNhat(sortTangDan(arr))}`;
+}
+
+
+
 // Tìm số chẳn cuối cùng nếu k có trả về -1
 function soChanCuoiCung(arr){
     for (var i =arr.length-1;i>=0;i--){
@@ -42,20 +86,13 @@ function soChanCuoiCung(arr){
         }
     }
 }
-// Thay đổi vị trí a,b
-function swap (arr,a,b){
-    var temp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = temp;
-    console.log(arr);
+document.getElementById('btn_soChanCuoiCung').onclick = function (){
+    document.getElementById('kq').innerHTML = `Số chẵn cuối cùng trong mảng là: ${soChanCuoiCung(arr)}`;
 }
-// Sắp xếp mảng (tăng dần)
-function sortTangDan (arr){
-    arr.sort(function(a,b){
-        return a-b;
-    })
-    return arr;
-}
+
+
+
+// First Prime (Số nguyên tố đầu tiên)
 function primeCheck(n) {
     var flag = true;
     var prime='';
@@ -88,8 +125,21 @@ function primeCheck(n) {
     }
     return prime;
 }
+document.getElementById('btn_firstPrime').onclick = function (){
+    for(var i =0;i<arr.length;i++){
+        if (primeCheck(arr[i])==='prime'){
+            document.getElementById('kq').innerHTML = `Số nguyên tố đầu tiên là ${arr[i]}`
+            break;
+        }
+        else {
+            document.getElementById('kq').innerHTML = -1;
+        }
+    }
+}
 
-// Số nguyên
+
+
+// Đếm số nguyên
 function intergerCount (arr){
     var count = 0;
     for (var i = 0; i < arr.length; i++){
@@ -99,6 +149,11 @@ function intergerCount (arr){
     }
     return count;
 }
+document.getElementById('btn_intergerCount').onclick = function (){
+    document.getElementById('kq').innerHTML = `có ${intergerCount(arr)} số nguyên trong mảng`
+}
+
+
 
 // Đếm số dương
 function positiveCount (arr){
@@ -120,77 +175,6 @@ function negativeCount (arr){
     }
     return count;
 }
-
-
-
-// Button
-
-
-// Submit array
-document.getElementById('btn_nhapMang').onclick = function(){
-    var arrItem = document.getElementById('txt_nhapMang').value*1;
-    nhapMang(arrItem);
-};
-
-// Spec array
-document.getElementById('btn_xemMang').onclick = function(){
-    document.getElementById('kq').innerHTML = `Mảng hiện tại: ${arr}`;
-};
-
-// Sum positive number
-document.getElementById('btn_tongDuong').onclick = function (){
-    document.getElementById('kq').innerHTML = `Tổng số dương trong mảng: ${tongSoDuong(arr)}`
-}
-
-// Count positive number
-document.getElementById('btn_demSoDuong').onclick = function (){
-    document.getElementById('kq').innerHTML = `Có ${demSoDuong(arr)} số dương trong mảng`
-}
-
-// Smallest number
-document.getElementById('btn_soNhoNhat').onclick = function (){
-    document.getElementById('kq').innerHTML = sortTangDan(arr)[0];
-}
-// Smallest positive number
-document.getElementById('btn_soDuongNhoNhat').onclick = function (){
-    document.getElementById('kq').innerHTML = `Số dương nhỏ nhất trong mảng là:${soDuongnhoNhat(sortTangDan(arr))}`;
-}
-// lastest even number
-document.getElementById('btn_soChanCuoiCung').onclick = function (){
-    document.getElementById('kq').innerHTML = `Số chẵn cuối cùng trong mảng là: ${soChanCuoiCung(arr)}`;
-}
-// Swap array
-document.getElementById('btn_swap').onclick = function(){
-    var a = document.getElementById('txt_1st').value*1;
-    var b = document.getElementById('txt_2nd').value*1;
-    swap(arr,a,b);
-    document.getElementById('kq').innerHTML = `Mảng mới: ${arr}`;
-}
-
-// Tănh dần
-document.getElementById('btn_tangDan').onclick = function(){
-    document.getElementById('kq').innerHTML = sortTangDan(arr);
-}
-
-
-// First Prime
-document.getElementById('btn_firstPrime').onclick = function (){
-    for(var i =0;i<arr.length;i++){
-        if (primeCheck(arr[i])==='prime'){
-            document.getElementById('kq').innerHTML = `Số nguyên tố đầu tiên là ${arr[i]}`
-            break;
-        }
-        else {
-            document.getElementById('kq').innerHTML = -1;
-        }
-    }
-}
-
-// Có bao nhiêu số nguyên
-document.getElementById('btn_intergerCount').onclick = function (){
-    document.getElementById('kq').innerHTML = `có ${intergerCount(arr)} số nguyên trong mảng`
-}
-
 // So sánh âm dương
 document.getElementById('btn_intergerCompare').onclick = function () {
     if (positiveCount(arr)>negativeCount(arr)){
@@ -201,4 +185,20 @@ document.getElementById('btn_intergerCompare').onclick = function () {
     } else {
         document.getElementById('kq').innerHTML = 'Số dương và âm = nhau';
     }
+}
+
+
+
+// Thay đổi vị trí a,b
+function swap (arr,a,b){
+    var temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+    console.log(arr);
+}
+document.getElementById('btn_swap').onclick = function(){
+    var a = document.getElementById('txt_1st').value*1;
+    var b = document.getElementById('txt_2nd').value*1;
+    swap(arr,a,b);
+    document.getElementById('kq').innerHTML = `Mảng mới: ${arr}`;
 }
